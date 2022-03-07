9bed6b4e2ff593bde5173da21731b2d34c599c21
hahaminja <haha_mj@naver.com>
2022-03-07 10:06:58 +0900

    업로드

--- Diptyque Paris _ Official Website_files/cookies.min(1).js
+++
@@ -1,5 +0,0 @@
-define(['jquery'],function($){'use strict';return{Defaults:{sessionOnly:false},set:function(name,value,days){var expires;if(!days){days=365;}
-var date=new Date();date.setTime(date.getTime()+(days*24*60*60*1000));expires="; expires="+date.toGMTString();if(this.Defaults.sessionOnly){expires="";}
-document.cookie=name+"="+value+expires+"; path=/";},get:function(name){var nameEQ=name+"=";var ca=document.cookie.split(';');for(var i=0;i<ca.length;i++){var c=ca[i];while(c.charAt(0)==' '){c=c.substring(1,c.length);}
-if(c.indexOf(nameEQ)==0){return c.substring(nameEQ.length,c.length);}}
-return null;},clear:function(name){document.cookie=name+"=; expires=Thu, 01-Jan-70 00:00:01 GMT; path=/";}}});
--- js/script.js
+++ js/script.js
@@ -3,6 +3,24 @@ $(document).ready(function(){
     $(".top-btn").click(function(){
         $("html,body").animate({scrollTop : 0}, 500)
     })
+
+    $(".slide-box").slick({
+        arrows: false ,
+        slidesToShow:4 ,
+        slidesToScroll: 1,
+        infinite: false ,
+        swipe:  true ,
+        swipeToSlide: true ,
+    })
+
+    $(".arrows-btn .prev-btn").click(function(){
+        $(".slide-box").slick('slickPrev')
+    })
+
+    $(".arrows-btn .next-btn").click(function(){
+        $(".slide-box").slick('slickNext')
+    })
+
 });
 
 $(window).scroll(function(){
